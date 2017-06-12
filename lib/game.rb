3df92f566aa865c0acc1da041eda9e6763479615
Adam Snow <adamjohnsnow@icloud.com>
2017-06-12 17:20:37 +0100

    better tests, now takes rectanglular grids

--- lib/game.rb
+++ lib/game.rb
@@ -2,14 +2,15 @@ class Game
   attr_reader :out_array, :in_array
 
   def initialize(array)
-    @array_length = array.length
-    @out_array = Array.new(@array_length){ Array.new(@array_length) }
-    @in_array = Array.new(@array_length + 2){ Array.new(@array_length + 2) }
+    @grid_height = array.length
+    @grid_width = array[0].length
+    @out_array = Array.new(@grid_height){ Array.new(@grid_width) }
+    @in_array = Array.new(@grid_height + 2){ Array.new(@grid_width + 2) }
     buffer(array)
   end
 
-  def tick(i)
-    i.times do
+  def tick(ticks)
+    ticks.times do
       @row = 1
       @col = 1
       do_all_cells
@@ -27,7 +28,7 @@ class Game
   end
 
   def do_all_cells
-    while @row <= @array_length
+    while @row <= @grid_height
       do_row_check
       @col = 1
       @row += 1
@@ -35,7 +36,7 @@ class Game
   end
 
   def do_row_check
-    while @col <= @array_length
+    while @col <= @grid_width
       build_neighbourhood
       @col += 1
     end
