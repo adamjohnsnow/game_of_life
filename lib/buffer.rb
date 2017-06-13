042de2c807af5a1b4a665da5b503e15453500d9e
Adam Snow <adamjohnsnow@icloud.com>
2017-06-13 17:29:54 +0100

    removed the word "output"

--- lib/buffer.rb
+++ lib/buffer.rb
@@ -1,11 +1,11 @@
 module Buffer
 
   def self.add_buffer(array)
-    output = Array.new(array.length + 2) { Array.new(array[0].length + 2, 0) }
+    buffered_grid = Array.new(array.length + 2) { Array.new(array[0].length + 2, 0) }
     array.each_with_index do |row, ind_x|
-      row.each_with_index { |x, ind_y| output[ind_x + 1][ind_y + 1] = x }
+      row.each_with_index { |x, ind_y| buffered_grid[ind_x + 1][ind_y + 1] = x }
     end
-    return output
+    return buffered_grid
   end
 
 end
